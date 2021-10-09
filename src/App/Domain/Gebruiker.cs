0e3f550b1c3f7b48234428405f0e66f58611c584
MohamedDilawar-hogent <mohamed.dilawar@student.hogent.be>
2021-10-09 15:33:53 +0200

    Project Web aangemaakt met domein klasses

---
+++ src/App/Domain/Gebruiker.cs
@@ -0,0 +1,34 @@
+﻿using Ardalis.GuardClauses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Gebruiker
+    {
+        public string Gebruikersnaam { get; private set; }
+        public DateTime Geboortedatum { get; private set; }
+        public string Email { get; private set; }
+        public GebruikerType Type { get; private set; }
+
+        public Gebruiker(string gebruikersnaam, DateTime geboortedatum, string email, GebruikerType type)
+        {
+            //Guard.Against.NullOrEmpty(gebruikersnaam, nameof(gebruikersnaam));
+            Gebruikersnaam = gebruikersnaam;
+            Geboortedatum = geboortedatum;
+            Email = email;
+            Type = type;
+        }
+
+      /* public void EditGebruiker(Gebruiker gebruiker)
+        {
+            this.Naam = gebruiker.Naam;
+            this.Geboortedatum = gebruiker.
+            
+        } */
+
+    }
+}
---
+++ src/App/Domain/Kunstwerk.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Domain
+{
+    public class Kunstwerk
+    {
+        public string Naam { get; private set; }
+        public DateTime Einddatum { get; private set; }
+        public double Prijs { get; private set; }
+        public string Beschrijving { get; private set; }
+        public ICollection<string> Fotos { get; private set; }
+
+        public Kunstwerk(string naam, DateTime einddatum, double prijs, string beschrijving, ICollection<string> fotos)
+        {
+            Naam = naam;
+            Einddatum = einddatum;
+            Prijs = prijs;
+            Beschrijving = beschrijving;
+            Fotos = fotos;
+        }
+
+
+
+    }
+}
