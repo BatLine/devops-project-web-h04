f7ce9adc8354592eeebfb27f43a57c25a9eb9ef8
MohamedDilawar-hogent <mohamed.dilawar@student.hogent.be>
2021-10-09 15:43:38 +0200

    Update Gebruiker.cs

--- src/App/Domain/Gebruiker.cs
+++ src/App/Domain/Gebruiker.cs
@@ -13,6 +13,7 @@ namespace Domain
         public DateTime Geboortedatum { get; private set; }
         public string Email { get; private set; }
         public GebruikerType Type { get; private set; }
+        public ICollection<Kunstwerk> Kunstwerken { get; set; }
 
         public Gebruiker(string gebruikersnaam, DateTime geboortedatum, string email, GebruikerType type)
         {
@@ -21,7 +22,10 @@ namespace Domain
             Geboortedatum = geboortedatum;
             Email = email;
             Type = type;
+            Kunstwerken = new List<Kunstwerk>();
         }
+
+
 
       /* public void EditGebruiker(Gebruiker gebruiker)
         {
