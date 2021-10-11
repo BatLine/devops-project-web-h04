c52a2416b2597785be9078060d0f252936cfab86
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-10-11 16:50:37 +0200

    DCD invullen (niet essentieel)

--- src/App/Domain/GebruikerType.cs
+++ src/App/Domain/GebruikerType.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain
+﻿namespace Domain
 {
     public enum GebruikerType
     {
-        Klant, Kunstenaar, Admin
+        Klant,
+        Kunstenaar,
+        Admin
     }
 }
