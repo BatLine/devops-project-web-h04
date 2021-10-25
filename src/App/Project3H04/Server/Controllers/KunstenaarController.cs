f8ecc89771ed997c332781183fcf6a6935e37ce0
MohamedDilawar-hogent <mohamed.dilawar@student.hogent.be>
2021-10-25 14:42:17 +0200

    Kunstenaar impl met @houssamBouazzaoui

--- src/App/Project3H04/Server/Controllers/KunstenaarController.cs
+++ src/App/Project3H04/Server/Controllers/KunstenaarController.cs
@@ -1,9 +1,11 @@
 ﻿using Domain;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; //deze voor de async toevoegen, de andere is kek
 using Project3H04.Server.Data;
 using Project3H04.Server.DTO;
 using System;
 using System.Collections.Generic;
+
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -16,87 +18,84 @@ namespace Project3H04.Server.Controllers
     public class KunstenaarController : ControllerBase
     {
         private readonly ApplicationDbcontext _context;
-        private readonly IEnumerable<Kunstenaar> _kunstenaars;
+        //private readonly DbSet<Kunstenaar> _kunstenaars;
 
-        //private readonly IConfiguration _configuration;
-
-        /* public DepartmentController(IConfiguration config)
-         {
-             this._configuration = config;
-         }*/
         public KunstenaarController(ApplicationDbcontext context)
         {
             _context = context;
-            _kunstenaars = (IEnumerable<Kunstenaar>)_context.Gebruikers.Where(x => x is Kunstenaar).ToList();
+            //_kunstenaars = (DbSet<Kunstenaar>)_context.Gebruikers.Where(x => x is Kunstenaar); 
+            //deze werkt nog niet => cast gaat niet, dus gwn in de method de filter doen
+
         }
 
-        // GET: api/<KunstwerkController>
+        //GET: api/<KunstwerkController>
         [HttpGet]
-        public IEnumerable<Kunstenaar> Get()
+        public async Task<IActionResult> GetKunstenaars()
         {
-            //werkt niet meer ViewData["Statussen"] = GetStatusSelectList(status);
-
-            return _kunstenaars;
+            //return Ok(await _context.Gebruikers.ToListAsync());
+            return Ok(await _context.Gebruikers.Where(x => x is Kunstenaar).ToListAsync());
+            //return Ok(await _kunstenaars.ToListAsync());
         }
 
         // GET api/<KunstwerkController>/5
-        [HttpGet("{id}")]
-        public ActionResult<Kunstenaar> Get(int id)
-        {
-            Kunstenaar kunst = _kunstenaars.SingleOrDefault(x => x.GebruikerId==id);
-            if(kunst == null )
-            return NotFound();
-
-            return kunst;
-        }
-
-        // POST api/<KunstwerkController>
-        [HttpPost]
-        public ActionResult<Kunstenaar> Post(Kunstenaar_DTO kunst)
-        {
-
-            Abonnement ab = new Abonnement(kunst.Abonnenment.StartDatum,
-                new AbonnementType(kunst.Abonnenment.AbonnementType.Naam, kunst.Abonnenment.AbonnementType.Verlooptijd
-                , kunst.Abonnenment.AbonnementType.Prijs));
-            Kunstenaar kunstenaarToCreate = new Kunstenaar(kunst.Gebruikersnaam, kunst.GeboorteDatum, kunst.Email, kunst.Details,ab);
-
-            _context.Gebruikers.Add(kunstenaarToCreate);
-            _context.SaveChanges();
-
-            return CreatedAtAction(nameof(Get), new { id = kunstenaarToCreate.GebruikerId },kunstenaarToCreate);
-        }
-
-        // PUT api/<KunstwerkController>/5
-        [HttpPut("{id}")]
-        public IActionResult Put(int id)
-        {
-            /* if (string.IsNullOrEmpty(naam))
-                 return BadRequest();*/
-
-            Kunstenaar kunst = _kunstenaars.SingleOrDefault(x => x.GebruikerId==id);
-            if (kunst == null)
-                return NotFound();
+        //[HttpGet("{id}")]
+        //public ActionResult<Gebruiker> Get(int id)
+        //{
+        //    if (id != null)
+        //        return _kunstenaars.SingleOrDefault(x => x.GebruikerId==id);
 
 
-            _context.Gebruikers.Update(kunst);
-            _context.SaveChanges();
-            return NoContent();
-        }
 
-        // DELETE api/<KunstwerkController>/5
-        [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
-        {
-            /*if (string.IsNullOrEmpty(naam))
-                return BadRequest();*/
+        //    return NotFound();
+        //}
 
-            Kunstenaar kunst = _kunstenaars.SingleOrDefault(x => x.GebruikerId == id);
-            if (kunst == null)
-                return NotFound();
+        // POST api/<KunstenaarController>
+        //[HttpPost]
+        //public ActionResult<Kunstenaar> Post(Kunstenaar_DTO kunst)
+        //{
 
-            _context.Gebruikers.Remove(kunst);
-            _context.SaveChanges();
-            return NoContent();
-        }
+        //    Abonnement ab = new Abonnement(kunst.Abonnenment.StartDatum,
+        //        new AbonnementType(kunst.Abonnenment.AbonnementType.Naam, kunst.Abonnenment.AbonnementType.Verlooptijd
+        //        , kunst.Abonnenment.AbonnementType.Prijs));
+        //    Kunstenaar kunstenaarToCreate = new Kunstenaar(kunst.Gebruikersnaam, kunst.GeboorteDatum, kunst.Email, kunst.Details,ab);
+
+        //    _context.Gebruikers.Add(kunstenaarToCreate);
+        //    _context.SaveChanges();
+
+        //    return CreatedAtAction(nameof(Get), new { id = kunstenaarToCreate.GebruikerId },kunstenaarToCreate);
+        //}
+
+        // PUT api/<KunstenaarController>/5
+        //[HttpPut("{id}")]
+        //public IActionResult Put(int id)
+        //{
+        //    /* if (string.IsNullOrEmpty(naam))
+        //         return BadRequest();*/
+
+        //    Kunstenaar kunst = _kunstenaars.SingleOrDefault(x => x.GebruikerId==id);
+        //    if (kunst == null)
+        //        return NotFound();
+
+
+        //    _context.Gebruikers.Update(kunst);
+        //    _context.SaveChanges();
+        //    return NoContent();
+        //}
+
+        //// DELETE api/<KunstenaarController>/5
+        //[HttpDelete("{id}")]
+        //public IActionResult Delete(int id)
+        //{
+        //    /*if (string.IsNullOrEmpty(naam))
+        //        return BadRequest();*/
+
+        //    Kunstenaar kunst = _kunstenaars.SingleOrDefault(x => x.GebruikerId == id);
+        //    if (kunst == null)
+        //        return NotFound();
+
+        //    _context.Gebruikers.Remove(kunst);
+        //    _context.SaveChanges();
+        //    return NoContent();
+        //}
     }
 }
