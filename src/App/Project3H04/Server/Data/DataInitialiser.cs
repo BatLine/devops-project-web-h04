6349bf135aec562a0a4158a3da3310243c919061
MohamedDilawar-hogent <mohamed.dilawar@student.hogent.be>
2021-10-25 17:33:20 +0200

    datainitialiserrr fix

--- src/App/Project3H04/Server/Data/DataInitialiser.cs
+++ src/App/Project3H04/Server/Data/DataInitialiser.cs
@@ -17,16 +17,28 @@ namespace Project3H04.Server.Data
 
         public void InitializeData()
         {
-            //_dbContext.Database.EnsureDeleted();
+            _dbContext.Database.EnsureDeleted();
             if (_dbContext.Database.EnsureCreated())
             {
                 //seeding the database, see DBContext
                 AbonnementType at = new AbonnementType("default",3,200);
                 Abonnement a = new Abonnement(DateTime.Now, at);
-                Kunstenaar k = new Kunstenaar("kunsternaar",DateTime.Now,"naam@gmail.com","details",a);
-                Kunstwerk kunst = new Kunstwerk("kunstwerk1", DateTime.Now, 200, "bla bla bla", new List<Foto> { new() { Pad = "artist1.PNG" } }, false, "hout", "kunstenaar1");
+                Abonnement a2 = new Abonnement(DateTime.Now, at);
+                Abonnement a3 = new Abonnement(DateTime.Now, at);
+                Kunstenaar k = new Kunstenaar("kunstenaar",DateTime.Now,"naam@gmail.com","details",a);
+                Kunstenaar b = new Kunstenaar("kunstenaar2", DateTime.Now, "naame@gmail.com", "details", a2);
+                Kunstenaar c = new Kunstenaar("babafafoey", DateTime.Now, "naamee@gmail.com", "details", a3);
+
+                Kunstwerk kunst = new Kunstwerk("kunstwerk1", DateTime.Now, 200, "bla bla bla", new List<Foto> { new() { Pad = "artist1.PNG" } }, false, "hout", "kunstenaar");
+                Kunstwerk kunst2 = new Kunstwerk("kunstwerk2", DateTime.Now, 300, "bla bla bla", new List<Foto> { new() { Pad = "artist2.PNG" } }, false, "hout", "kunstenaar2");
+                Kunstwerk kunst3 = new Kunstwerk("tha bababoey", DateTime.Now, 1500, "bla bla bla", new List<Foto> { new() { Pad = "artist3.PNG" } }, false, "hout", "babafafoey");
                 k.AddKunstwerk(kunst);
+                b.AddKunstwerk(kunst2);
+                c.AddKunstwerk(kunst3);
                 _dbContext.Gebruikers.Add(k);
+                _dbContext.Gebruikers.Add(b);
+                _dbContext.Gebruikers.Add(c);
+                k.AddKunstwerk(kunst);
                 _dbContext.SaveChanges();
             }
         }
