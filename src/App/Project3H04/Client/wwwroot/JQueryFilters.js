5b6b41f7b3a122475e3e987ed5dfe2f1f0a4fb30
Aysha <aysha.arshad@student.hogent.be>
2021-10-26 21:36:18 +0200

    Add JQuery

---
+++ src/App/Project3H04/Client/wwwroot/JQueryFilters.js
@@ -0,0 +1,71 @@
+﻿
+
+
+function initializeFilters() {
+    //select input fields with class form-check
+
+    var $filterCheckboxes = $("input.form-check-input:checkbox");
+
+    //when $filterCheckboxes is changed this function will be called
+
+    var filterFunc = function () {
+        var selectedFilters = {};
+
+        $filterCheckboxes.filter(':checked').each(function () {
+
+            if (!selectedFilters.hasOwnProperty(this.name)) {
+                selectedFilters[this.name] = [];
+            }
+            // push selected filter value in SelectedFilters
+
+            selectedFilters[this.name].push(this.value);
+
+        });
+
+        // create a collection containing all of the filterable elements: <div class="artwork">
+
+        var $filteredResults = $('.artworks');
+
+        // loop over the selected filter name -> (array) values pairs
+
+        $.each(selectedFilters, function (name, filterValues) {
+
+            // $filteredResults contains the gallery photos divs
+
+            $filteredResults = $filteredResults.filter(function () {
+
+                var matched = false
+                var currentFilterValues = $(this).data('category').split(' ');
+
+                // currentFilterValues = category values
+                // loop over each category value in the current data-category of the gallery
+
+                $.each(currentFilterValues, function (_, currentFilterValue) {
+                    // if the current category exists in the selected filters array
+                    // set matched to true, and stop looping
+                    // set of filters, we only need to match once
+                    // $inArray will return -1 if not found
+
+                    if ($.inArray(currentFilterValue, filterValues) != -1) {
+                        matched = true;
+                        return false;
+                    }
+                });
+
+
+                // if matched is true the current .artwork element is returned
+
+                return matched;
+
+            }
+            );
+
+        });
+        $('.artworks').hide().filter($filteredResults).show();
+    }
+
+    //click event
+
+    $filterCheckboxes.on('click', filterFunc);
+
+};
--- src/App/Project3H04/Server/Data/DataInitialiser.cs
+++ src/App/Project3H04/Server/Data/DataInitialiser.cs
@@ -21,15 +21,15 @@ namespace Project3H04.Server.Data
             if (_dbContext.Database.EnsureCreated())
             {
                 //seeding the database, see DBContext
-                AbonnementType at = new AbonnementType("default",3,200);
+                AbonnementType at = new AbonnementType("default", 3, 200);
                 Abonnement a = new Abonnement(DateTime.Now, at);
                 Abonnement a2 = new Abonnement(DateTime.Now, at);
                 Abonnement a3 = new Abonnement(DateTime.Now, at);
-                Kunstenaar k = new Kunstenaar("kunstenaar",DateTime.Now,"naam@gmail.com","details",a);
+                Kunstenaar k = new Kunstenaar("kunstenaar", DateTime.Now, "naam@gmail.com", "details", a);
                 Kunstenaar b = new Kunstenaar("kunstenaar2", DateTime.Now, "naame@gmail.com", "details", a2);
                 Kunstenaar c = new Kunstenaar("babafafoey", DateTime.Now, "naamee@gmail.com", "details", a3);
 
-                Kunstwerk kunst = new Kunstwerk("kunstwerk1", DateTime.Now, 200, "bla bla bla", new List<Foto> { new() { Pad = "artist1.PNG" } }, false, "hout", "kunstenaar");
+                Kunstwerk kunst = new Kunstwerk("kunstwerk1", DateTime.Now, 200, "bla bla bla", new List<Foto> { new() { Pad = "artist1.PNG" } }, false, "metaal", "kunstenaar");
                 Kunstwerk kunst2 = new Kunstwerk("kunstwerk2", DateTime.Now, 300, "bla bla bla", new List<Foto> { new() { Pad = "artist2.PNG" } }, false, "hout", "kunstenaar2");
                 Kunstwerk kunst3 = new Kunstwerk("tha bababoey", DateTime.Now, 1500, "bla bla bla", new List<Foto> { new() { Pad = "artist3.PNG" } }, false, "hout", "babafafoey");
                 k.AddKunstwerk(kunst);
