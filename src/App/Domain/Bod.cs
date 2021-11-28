ede545f22d702fe67d48f2ede307a8087c1c163d
Gilles De Pessemier <gilles.depessemier@gmail.com>
2021-11-28 21:26:31 +0100

    Klasses fixes

--- src/App/Domain/Bod.cs
+++ src/App/Domain/Bod.cs
@@ -5,17 +5,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain
-{
-    public class Bod
-    {
+namespace Domain {
+    public class Bod {
         public int Id { get; private set; }
-        public Klant Klant{ get; private set; }
+        public Klant Klant { get; private set; }
         public decimal BodPrijs { get; set; }
         public DateTime Datum { get; set; }
+        public int KlantId { get; private set; }
 
         public Bod(Klant klant, decimal bodPrijs, DateTime datum) {
             Klant = Guard.Against.Null(klant, nameof(klant));
+            KlantId = klant.GebruikerId;
             BodPrijs = Guard.Against.Null(bodPrijs, nameof(bodPrijs));
             Datum = Guard.Against.Null(datum, nameof(datum));
         }
--- src/App/Domain/Gebruiker.cs
+++ src/App/Domain/Gebruiker.cs
@@ -5,19 +5,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Domain
-{
-    public class Gebruiker
-    {
+namespace Domain {
+    public class Gebruiker {
         public string Gebruikersnaam { get; private set; }
         public DateTime Geboortedatum { get; private set; }
         public string Email { get; private set; }
         public int GebruikerId { get; private set; }
         public DateTime DatumCreatie { get;  set; }
         public string FotoPad { get; set; }
-        public Gebruiker(string gebruikersnaam, DateTime geboortedatum, string email, string fotoPad)
-
-        {
+        
+        public Gebruiker(string gebruikersnaam, DateTime geboortedatum, string email, string fotoPad) {
             //Guard.Against.NullOrEmpty(gebruikersnaam, nameof(gebruikersnaam));
             Gebruikersnaam = Guard.Against.NullOrWhiteSpace(gebruikersnaam, nameof(gebruikersnaam));
             Geboortedatum = Guard.Against.Null(geboortedatum, nameof(geboortedatum));
@@ -26,8 +23,8 @@ namespace Domain
             this.FotoPad = fotoPad;
             //this.FotoPad = Guard.Against.NullOrEmpty(fotoPad, nameof(fotoPad));
         }
-        public Gebruiker()
-        {
+
+        public Gebruiker() {
 
         }
 
