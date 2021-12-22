28a26cb136351c1785d2e81f5d72cbfa04010311
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-12-22 15:48:56 +0100

    alle kunstwerk api calls in één klasse

--- src/App/Project3H04/Client/Services/KunstwerkService.cs
+++ src/App/Project3H04/Client/Services/KunstwerkService.cs
@@ -1,4 +1,6 @@
-﻿using Project3H04.Shared.Kunstwerken;
+﻿using Project3H04.Client.Extensions;
+using Project3H04.Client.Shared;
+using Project3H04.Shared.Kunstwerken;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Json;
@@ -9,16 +11,26 @@ namespace Project3H04.Client.Services
     public class KunstwerkService
     {
         private readonly HttpClient httpClient;
+        private readonly PublicClient publicClient; //Deze http PublicClient gebruiken voor Anonymous--> 
         private const string endpoint = "api/kunstwerk";
 
-        public KunstwerkService(HttpClient httpClient)
+        public KunstwerkService(HttpClient httpClient, PublicClient publicClient)
         {
             this.httpClient = httpClient;
+            this.publicClient = publicClient;
         }
 
-        public Task<List<string>> GetMaterialenAsync(int aantal)
+        public async Task<KunstwerkResponse.Index> GetIndexAsync()
         {
-            return httpClient.GetFromJsonAsync<List<string>>($"{endpoint}/materiaal/{aantal}");
+            var response = await publicClient.Client.GetFromJsonAsync<KunstwerkResponse.Index>($"{endpoint}");
+            return response;
+        }
+
+        public async Task<KunstwerkResponse.Index> GetIndexAsync(Kunstwerk_DTO.Filter request)
+        {
+            var queryParameters = request.GetQueryString();
+            var response = await publicClient.Client.GetFromJsonAsync<KunstwerkResponse.Index>($"{endpoint}?{queryParameters}");
+            return response;
         }
 
         public async Task<KunstwerkResponse.Edit> EditAsync(Kunstwerk_DTO.Edit model)
@@ -33,6 +45,16 @@ namespace Project3H04.Client.Services
             var response = await httpClient.PostAsJsonAsync($"{endpoint}", model);
             response.EnsureSuccessStatusCode();
             return await response.Content.ReadFromJsonAsync<KunstwerkResponse.Create>();
+        }
+
+        public Task<int> GetAantalKunstAsync()
+        {
+            return publicClient.Client.GetFromJsonAsync<int>($"{endpoint}/aantalKunst");
+        }
+
+        public Task<List<string>> GetMaterialenAsync(int aantal)
+        {
+            return publicClient.Client.GetFromJsonAsync<List<string>>($"{endpoint}/materiaal/{aantal}");
         }
     }
 }
