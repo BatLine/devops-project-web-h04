7bedf2eeab12026da86df779b75151a77364a115
LeonPaelinck <60927864+LeonPaelinck@users.noreply.github.com>
2021-12-22 14:57:16 +0100

    bugfix indexResponse

---
+++ src/App/Project3H04/Client/Services/KunstwerkService.cs
@@ -0,0 +1,38 @@
+﻿using Project3H04.Shared.Kunstwerken;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace Project3H04.Client.Services
+{
+    public class KunstwerkService
+    {
+        private readonly HttpClient httpClient;
+        private const string endpoint = "api/kunstwerk";
+
+        public KunstwerkService(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+        }
+
+        public Task<List<string>> GetMaterialenAsync(int aantal)
+        {
+            return httpClient.GetFromJsonAsync<List<string>>($"{endpoint}/materiaal/{aantal}");
+        }
+
+        public async Task<KunstwerkResponse.Edit> EditAsync(Kunstwerk_DTO.Edit model)
+        {
+            var response = await httpClient.PutAsJsonAsync($"{endpoint}", model);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<KunstwerkResponse.Edit>();
+        }
+
+        public async Task<KunstwerkResponse.Create> CreateAsync(Kunstwerk_DTO.Create model)
+        {
+            var response = await httpClient.PostAsJsonAsync($"{endpoint}", model);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<KunstwerkResponse.Create>();
+        }
+    }
+}
