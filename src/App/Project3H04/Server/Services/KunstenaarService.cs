efc371d7f0f128a395b1639ba737298ee85a399c
Aysha <aysha.arshad@student.hogent.be>
2021-12-25 19:57:54 +0100

    fix geboortedatum in Account

--- src/App/Project3H04/Server/Services/KunstenaarService.cs
+++ src/App/Project3H04/Server/Services/KunstenaarService.cs
@@ -9,26 +9,32 @@ using System.Linq;
 using System.Threading.Tasks;
 using Project3H04.Shared.DTO;
 
-namespace Project3H04.Server.Services {
-    public class KunstenaarService : IKunstenaarService {
+namespace Project3H04.Server.Services
+{
+    public class KunstenaarService : IKunstenaarService
+    {
         private readonly ApplicationDbcontext dbContext;
 
-        public KunstenaarService(ApplicationDbcontext dbContext) {
+        public KunstenaarService(ApplicationDbcontext dbContext)
+        {
             this.dbContext = dbContext;
         }
 
-        public async Task<KunstenaarResponse.Detail> GetDetailAsync(int id) {
+        public async Task<KunstenaarResponse.Detail> GetDetailAsync(int id)
+        {
             //kunstwerk.fotos nog includen !!!
 
             var x = (Kunstenaar)dbContext.Gebruikers.OfType<Kunstenaar>().Include(k => k.Kunstwerken).ThenInclude(x => x.Fotos).SingleOrDefault(x => x.GebruikerId == id);
             //include van fotos ...
-            var k = await Task.Run(() => new Kunstenaar_DTO {
+            var k = await Task.Run(() => new Kunstenaar_DTO
+            {
                 Gebruikersnaam = x.Gebruikersnaam,
                 GebruikerId = x.GebruikerId,
                 Details = x.Details,
                 Email = x.Email,
                 //deze nog goe omzette
-                Kunstwerken = x.Kunstwerken.Select(x => new Kunstwerk_DTO.Index {
+                Kunstwerken = x.Kunstwerken.Select(x => new Kunstwerk_DTO.Index
+                {
                     Id = x.Id,
                     Naam = x.Naam,
                     HoofdFoto = new Foto_DTO(x.Fotos.FirstOrDefault().Naam, x.Fotos.FirstOrDefault().Locatie), //enkel eerste foto is nodig voor index
@@ -41,30 +47,36 @@ namespace Project3H04.Server.Services {
             return new KunstenaarResponse.Detail() { Kunstenaar = k };
         }
 
-        public async Task<KunstenaarResponse.Detail> GetKunstenaarByEmail(string email) {
+        public async Task<KunstenaarResponse.Detail> GetKunstenaarByEmail(string email)
+        {
             var x = (Kunstenaar)dbContext.Gebruikers.OfType<Kunstenaar>().Include(k => k.Kunstwerken).ThenInclude(x => x.Fotos).Include(x => x.Abonnenment).ThenInclude(x => x.AbonnementType).SingleOrDefault(x => x.Email == email);
             //include van fotos ...
-            if(x == null)
+            if (x == null)
             {
                 return new KunstenaarResponse.Detail();
             }
-            var k = await Task.Run(() => new Kunstenaar_DTO {
+            var k = await Task.Run(() => new Kunstenaar_DTO
+            {
                 Gebruikersnaam = x.Gebruikersnaam,
                 GebruikerId = x.GebruikerId,
+                GeboorteDatum = x.Geboortedatum,
                 Details = x.Details,
                 Email = x.Email,
                 //deze nog goe omzette
-                Kunstwerken = x.Kunstwerken.Select(x => new Kunstwerk_DTO.Index {
+                Kunstwerken = x.Kunstwerken.Select(x => new Kunstwerk_DTO.Index
+                {
                     Id = x.Id,
                     Naam = x.Naam,
                     HoofdFoto = new Foto_DTO(x.Fotos.FirstOrDefault().Naam, x.Fotos.FirstOrDefault().Locatie), //enkel eerste foto is nodig voor index
                     Prijs = x.Prijs
                 }).ToList(),
-                Abonnement = new Abonnement_DTO {
+                Abonnement = new Abonnement_DTO
+                {
                     Id = x.AbonnenmentId,
                     StartDatum = x.Abonnenment.StartDatum,
                     EindDatum = x.Abonnenment.EindDatum,
-                    AbonnementType = new AbonnementType_DTO {
+                    AbonnementType = new AbonnementType_DTO
+                    {
                         Naam = x.Abonnenment.AbonnementType.Naam,
                         Verlooptijd = x.Abonnenment.AbonnementType.Verlooptijd,
                         Prijs = x.Abonnenment.AbonnementType.Prijs
@@ -77,20 +89,24 @@ namespace Project3H04.Server.Services {
             return new KunstenaarResponse.Detail() { Kunstenaar = k };
         }
 
-        public async Task<KunstenaarResponse.Index> GetKunstenaars(string term, int take, bool recentArtists) {
+        public async Task<KunstenaarResponse.Index> GetKunstenaars(string term, int take, bool recentArtists)
+        {
             term ??= "";
-            
+
             var kunstenaars = await dbContext.Gebruikers.OfType<Kunstenaar>()
-                .Include(x=> x.Kunstwerken)
-                .ThenInclude(x=>x.Fotos)
-                .Select(x => new Kunstenaar_DTO {
+                .Include(x => x.Kunstwerken)
+                .ThenInclude(x => x.Fotos)
+                .Select(x => new Kunstenaar_DTO
+                {
                     Gebruikersnaam = x.Gebruikersnaam,
                     GebruikerId = x.GebruikerId,
                     DatumCreatie = x.DatumCreatie,
                     Fotopad = x.FotoPad,
+                    GeboorteDatum = x.Geboortedatum,
                     GeboortedatumShort = x.Geboortedatum.ToShortDateString(),
                     Details = x.Details,
-                    Kunstwerken = x.Kunstwerken.Select(x => new Kunstwerk_DTO.Index {
+                    Kunstwerken = x.Kunstwerken.Select(x => new Kunstwerk_DTO.Index
+                    {
                         Id = x.Id,
                         Naam = x.Naam,
                         HoofdFoto = new Foto_DTO(x.Fotos.FirstOrDefault().Naam, x.Fotos.FirstOrDefault().Locatie),
--- src/App/Project3H04/Shared/DTO/Gebruiker_DTO.cs
+++ src/App/Project3H04/Shared/DTO/Gebruiker_DTO.cs
@@ -5,8 +5,10 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Project3H04.Shared.DTO {
-    public class Gebruiker_DTO {
+namespace Project3H04.Shared.DTO
+{
+    public class Gebruiker_DTO
+    {
         public int GebruikerId { get; set; }
         public string Gebruikersnaam { get; set; }
         public DateTime GeboorteDatum { get; set; }
@@ -15,12 +17,15 @@ namespace Project3H04.Shared.DTO {
         public string Fotopad { get; set; }
         public string Details { get; set; }
 
-        public class Validator : AbstractValidator<Gebruiker_DTO> {
-            public Validator() {
+        public class Validator : AbstractValidator<Gebruiker_DTO>
+        {
+            public Validator()
+            {
                 RuleFor(x => x.Gebruikersnaam).NotEmpty().OverridePropertyName("Username");
                 RuleFor(x => x.GeboorteDatum).NotEmpty().OverridePropertyName("Birthdate");
                 RuleFor(x => x.Email).NotEmpty().OverridePropertyName("Email");
-                RuleFor(x => x.Details).NotEmpty(); 
+                RuleFor(x => x.Details).NotEmpty();
+                RuleFor(x => x.GeboorteDatum).LessThanOrEqualTo(DateTime.UtcNow).WithMessage("Cannot use a future date.");
                 //RuleFor(x=>x.Fotopad).NotEmpty().OverridePropertyName("Images");
             }
         }
